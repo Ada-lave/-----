d2357467c31195fe7c24db28a1eb3d2e42cfe8ea
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2023-02-28 11:19:44 +0500

    test

--- Farm_project/Earth/models.py
+++ Farm_project/Earth/models.py
@@ -4,4 +4,7 @@ class DataVegetables(models.Model):
     title = models.CharField(max_length=65)
     price = models.IntegerField(max_length=100)
     description = models.TextField(max_length=1000)
-    
+
+class User(models.model):
+    name = models.CharField(max_length=65)
+    age = models.CharField(max_length=1000)
